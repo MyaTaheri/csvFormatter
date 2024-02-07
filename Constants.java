05d7d378b3ce9a2f83387d04ccdea97a26189588
MyaTaheri <91988696+MyaTaheri@users.noreply.github.com>
2024-02-07 16:59:37 -0500

    first commit

---
+++ Constants.java
@@ -0,0 +1,14 @@
+public class Constants
+{
+    public int autoAmp = 0;
+    public int autoShots = 0;
+    public int autoPercent = 0;
+    public int autoPosition = 0;
+
+    public int telAmp = 0;
+    public int telShots = 0;
+    public int telPercent = 0;
+
+    public int climbingAbilities = 0;
+    
+}
